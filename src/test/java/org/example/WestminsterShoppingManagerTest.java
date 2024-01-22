03e59fbd5ae66b6cb77e98651b09ebdaf8385e6e
sandali24 <sandalisiri58@gmail.com>
2024-01-22 12:11:31 +0530

    GUI

---
+++ src/test/java/org/example/WestminsterShoppingManagerTest.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class WestminsterShoppingManagerTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    void displayMenu() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+
+    @Test
+    void addElectronics() {
+    }
+
+    @Test
+    void addClothing() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void printListOfProducts() {
+    }
+
+    @Test
+    void saveIntoFile() {
+    }
+
+    @Test
+    void saveProductsDetails() {
+    }
+}
